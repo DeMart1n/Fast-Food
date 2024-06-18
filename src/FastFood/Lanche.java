9b3d1afe495089dbab3c8d82d3e49a565147fb36
demartinc <demartincaua@gmail.com>
2024-06-18 19:52:25 -0300

    .

---
+++ src/FastFood/Lanche.java
@@ -0,0 +1,12 @@
+package FastFood;
+
+public class Lanche {
+    private String nome;
+    private String descricao;
+    private double preco;
+        public Lanche(String nome, String descricao, double preco) {
+            this.nome = nome;
+            this.descricao = descricao;
+            this.preco = preco;
+    }
+}
